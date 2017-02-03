267bb94888a615170e4d682445d5a05cac9cdb0e
(Student) - Peter Daly <c00205483@ITCARLOW.IE>
2017-02-03 12:47:37 +0000

    Added a slider

--- include/Options.h
+++ include/Options.h
@@ -7,42 +7,54 @@
 #include "Xbox360Controller.h"
 #include "MainMenu.h"
 
-class Game;
+class Game; //Declares game for circular reference
 
-enum optionButton
+enum optionButton	//An enum for which button is selected
 {
+	//All the option buttons
 	Option1,
 	Option2,
 	Option3
 };
 
+//The option screen class
 class OptionsScreen
 {
 public:
-	OptionsScreen(Game& game, sf::Font &font);
-	~OptionsScreen();
+	OptionsScreen(Game& game, sf::Font &font);	//Constructor
+	~OptionsScreen();	//De-constructor
+	//Function to update the options creen
 	void update(GamePadState m_state, sf::Time deltaTime, Xbox360Controller2 m_controller);
+	//function to render the options screen
 	void render(sf::RenderWindow& window);
+	//Function to edit the text based on if theyre selected
+	//And to check if the player is moving between buttons
 	void checkButtonSelected(GamePadState m_state, Xbox360Controller2 m_controller);
+	//Function to check if buttons are selected 
 	void selectedButton(GamePadState m_state, Xbox360Controller2 m_controller);
 
 private:
-	Game *m_game;
-	optionButton m_oButton = optionButton::Option1;
-	bool m_transitionToMenu;
+	Game *m_game; //The game object so you can change game states
+	optionButton m_oButton = optionButton::Option1;	//The enum starts at option 1
+	//Bool for whether or not the options screen is transitioning to the menu
+	bool m_transitionToMenu;	
+	//Bool for whether or not the options screen is transitioning in from the menu
 	bool m_transitionFromMenu = true;
-	sf::Texture m_backgroundTexture;
-	sf::Sprite m_backgroundSprite;
 
-	sf::Texture m_buttonTex;
+	sf::Texture m_backgroundTexture;	//Background texture
+	sf::Sprite m_backgroundSprite;	//Background sprite
 
+	sf::Texture m_buttonTex;	//Texture for the button
+
+	//The two button sprites
 	sf::Sprite m_button1;
 	sf::Sprite m_button2;
-	sf::Sprite m_button3;
 
+	sf::RectangleShape m_slider;	//The slider
+
+	//The two pieces of text
 	sf::Text m_button1Text;
 	sf::Text m_button2text;
-	sf::Text m_button3text;
 };
 
 #endif // !OPTIONS
--- src/Options.cpp
+++ src/Options.cpp
@@ -1,12 +1,16 @@
 #include "..\include\Options.h"
 
+
+//Constructor for the options screen
 OptionsScreen::OptionsScreen(Game& game, sf::Font &font) :
 	m_game(&game),
-	m_button1Text("YEAH", font, 28),
-	m_button2text("WAHU", font, 28),
-	m_button3text("BOYO", font, 28)
+
+	//Set up the three pieces of text
+	m_button1Text("MEME", font, 28),
+	m_button2text("BACK", font, 28)
 	
 {
+	//check to make sure the image loads correctly
 	if (!m_backgroundTexture.loadFromFile(".\\resources\\images\\startBG.jpg"))
 	{
 		{
@@ -15,7 +19,7 @@ OptionsScreen::OptionsScreen(Game& game, sf::Font &font) :
 		}
 
 	}
-
+	//check to make sure the image loads correctly
 	if (!m_buttonTex.loadFromFile(".\\resources\\images\\Button.png"))
 	{
 		{
@@ -24,132 +28,163 @@ OptionsScreen::OptionsScreen(Game& game, sf::Font &font) :
 		}
 	}
 
+	//Assigns the background texture to the background sprite
 	m_backgroundSprite.setTexture(m_backgroundTexture);
 
+	//Assigns the button texture to the three button sprites
 	m_button1.setTexture(m_buttonTex);
 	m_button2.setTexture(m_buttonTex);
-	m_button3.setTexture(m_buttonTex);
 
-	m_backgroundSprite.setOrigin(450, 300);
-	m_backgroundSprite.setPosition(450, 300);
-	m_backgroundSprite.setScale(1.04, 1.04);
+	m_backgroundSprite.setOrigin(450, 300);	//Sets the backgrounds origin
+	m_backgroundSprite.setPosition(450, 300);	//Sets the backgrounds position
+	m_backgroundSprite.setScale(1.04, 1.04);	//Sets the backgrounds scale
 
+	//Sets the position of the three buttons
 	m_button1.setPosition(1000, 200);
-	m_button2.setPosition(1000, 300);
-	m_button3.setPosition(1000, 400);
+	m_button2.setPosition(1000, 400);
+
+	//Sets the position of the three texts
 	m_button1Text.setPosition(1035, 205);
-	m_button2text.setPosition(1035, 305);
-	m_button3text.setPosition(1035, 405);
+	m_button2text.setPosition(1035, 405);
+
+	m_slider.setFillColor(sf::Color(0, 0, 0));	//Sets the initial slider colour to black
+	m_slider.setPosition(1000, 300);	//Sets the sliders position
+	m_slider.setSize(sf::Vector2f(180, 30));	//Sets the size fo the slider
 
 }
+
+//Destructor function for the options screen
 OptionsScreen::~OptionsScreen()
 {
 }
 
+
+//Function to update the options screen
 void OptionsScreen::update(GamePadState m_state, sf::Time deltaTime, Xbox360Controller2 m_controller)
 {
-	checkButtonSelected(m_state, m_controller);
-	selectedButton(m_state, m_controller);
+	checkButtonSelected(m_state, m_controller);	//Calls the function to check which button is selected
+	selectedButton(m_state, m_controller);	//Calls the function to check if a button has been clicked
 
+	//If the options is transitioning in from the menu
 	if (m_transitionFromMenu)
 	{
-		if (m_button1.getPosition().x > 370)
+		if (m_button1.getPosition().x > 370)	//Checks if the buttons havent reached their destination
 		{
+			//Move all the button sprites slightly to the left
 			m_button1.setPosition(m_button1.getPosition().x - 1, m_button1.getPosition().y);
 			m_button2.setPosition(m_button2.getPosition().x - 1, m_button2.getPosition().y);
-			m_button3.setPosition(m_button3.getPosition().x - 1, m_button3.getPosition().y);
+			m_slider.setPosition(m_slider.getPosition().x - 1, m_slider.getPosition().y);
 
+			//Move all the button texts slightly to the left
 			m_button1Text.setPosition(m_button1Text.getPosition().x - 1, m_button1Text.getPosition().y);
 			m_button2text.setPosition(m_button2text.getPosition().x - 1, m_button2text.getPosition().y);
-			m_button3text.setPosition(m_button3text.getPosition().x - 1, m_button3text.getPosition().y);
 		}
-		else
+		else   //If the buttons are at the right location	
 		{
-			m_transitionFromMenu = false;
+			m_transitionFromMenu = false;	//Stop the transition
 		}
 	}
 
+	//If the options is transitioning to the menu
 	if (m_transitionToMenu)
 	{
-		if (m_button1.getPosition().x < 1000)
+		if (m_button1.getPosition().x < 1000)	//Checks if the buttons havent yet moved off the screen
 		{
+			//Moves the buttons to the right
 			m_button1.setPosition(m_button1.getPosition().x + 1, m_button1.getPosition().y);
 			m_button2.setPosition(m_button2.getPosition().x + 1, m_button2.getPosition().y);
-			m_button3.setPosition(m_button3.getPosition().x + 1, m_button3.getPosition().y);
+			m_slider.setPosition(m_slider.getPosition().x + 1, m_slider.getPosition().y);
 
+			//Moves the texts to the right
 			m_button1Text.setPosition(m_button1Text.getPosition().x + 1, m_button1Text.getPosition().y);
 			m_button2text.setPosition(m_button2text.getPosition().x + 1, m_button2text.getPosition().y);
-			m_button3text.setPosition(m_button3text.getPosition().x + 1, m_button3text.getPosition().y);
+			
 		}
-		else
+		else   //If the buttons are at the right location
 		{
-			m_transitionFromMenu = true;
-			m_transitionToMenu = false;
-			m_game->m_screen = MenuState::MenuScreen;
+			m_transitionFromMenu = true;	//Sets this to true so it is ready when the player goes back to options
+			m_transitionToMenu = false;	//Stops the transition
+			m_game->m_screen = MenuState::MenuScreen;	//Sets the game back to the menu
 		}
 	}
 }
 
+//Function to render the options screen
 void OptionsScreen::render(sf::RenderWindow & window)
 {
-	window.clear(sf::Color(220, 20, 60));
+	window.clear(sf::Color(220, 20, 60));	//Clears the screen with a nice red colour
+
 	window.draw(m_backgroundSprite);		//Draws text to the screen
+
+	//draws all the buttons
 	window.draw(m_button1);
 	window.draw(m_button2);
-	window.draw(m_button3);
+
+	window.draw(m_slider);	//draws the slider
+
+	//Draws all the text
 	window.draw(m_button1Text);
 	window.draw(m_button2text);
-	window.draw(m_button3text);
+
 	window.display();	//Displays the screen
 }
 
+//Function to edit the text based on if theyre selected
+//And to check if the player is moving between buttons
 void OptionsScreen::checkButtonSelected(GamePadState m_state, Xbox360Controller2 m_controller)
 {
+	//Switch statement to check which button is selected
 	switch (m_oButton)
 	{
 	case Option1:
-		m_button1Text.setColor(sf::Color(236, 0, 24));
-		m_button2text.setColor(sf::Color(0, 0, 0));
-		m_button3text.setColor(sf::Color(0, 0, 0));
+		m_slider.setFillColor(sf::Color(0, 0, 0));
+		m_button1Text.setColor(sf::Color(236, 0, 24));	//Sets the current selected button to red
+		m_button2text.setColor(sf::Color(0, 0, 0));		//Sets the other two back to black
 
+		//Checks if the player is trying to select the button below the current one
 		if ((m_state.dpadDown && !m_controller.m_previousState.dpadDown) || (m_state.LeftThumbStick.y > 50 && m_controller.m_previousState.LeftThumbStick.y < 50))
 		{
-			m_oButton = optionButton::Option2;
+			m_oButton = optionButton::Option2;	//Changes the current button
 		}
 
+		//Checks if the player is trying to select the button above the current one
 		if ((m_state.dpadUp && !m_controller.m_previousState.dpadUp) || (m_state.LeftThumbStick.y < -50 && m_controller.m_previousState.LeftThumbStick.y > -50))
 		{
-			m_oButton = optionButton::Option3;
+			m_oButton = optionButton::Option3;	//Changes the current button
 		}
 		break;
 	case Option2:
+		m_slider.setFillColor(sf::Color(236, 0, 24));
 		m_button1Text.setColor(sf::Color(0, 0, 0));
-		m_button2text.setColor(sf::Color(236, 0, 24));
-		m_button3text.setColor(sf::Color(0, 0, 0));
+		m_button2text.setColor(sf::Color(0, 0, 0));	//Sets the current selected button to red
 
+		//Checks if the player is trying to select the button below the current one
 		if ((m_state.dpadDown && !m_controller.m_previousState.dpadDown) || (m_state.LeftThumbStick.y > 50 && m_controller.m_previousState.LeftThumbStick.y < 50))
 		{
-			m_oButton = optionButton::Option3;
+			m_oButton = optionButton::Option3;	//Changes the current button
 		}
 
+		//Checks if the player is trying to select the button above the current one
 		if ((m_state.dpadUp && !m_controller.m_previousState.dpadUp) || (m_state.LeftThumbStick.y < -50 && m_controller.m_previousState.LeftThumbStick.y > -50))
 		{
-			m_oButton = optionButton::Option1;
+			m_oButton = optionButton::Option1;	//Changes the current button
 		}
 		break;
 	case Option3:
-		m_button1Text.setColor(sf::Color(0, 0, 0));
-		m_button2text.setColor(sf::Color(0, 0, 0));
-		m_button3text.setColor(sf::Color(236, 0, 24));
+		m_slider.setFillColor(sf::Color(0, 0, 0));
+		m_button1Text.setColor(sf::Color(0, 0, 0));	//Sets the other two back to black
+		m_button2text.setColor(sf::Color(236, 0, 24));	//Sets the current selected button to red
 
+		//Checks if the player is trying to select the button below the current one
 		if ((m_state.dpadDown && !m_controller.m_previousState.dpadDown) || (m_state.LeftThumbStick.y > 50 && m_controller.m_previousState.LeftThumbStick.y < 50))
 		{
-			m_oButton = optionButton::Option1;
+			m_oButton = optionButton::Option1;	//Changes the current button
 		}
 
+		//Checks if the player is trying to select the button above the current one
 		if ((m_state.dpadUp && !m_controller.m_previousState.dpadUp) || (m_state.LeftThumbStick.y < -50 && m_controller.m_previousState.LeftThumbStick.y > -50))
 		{
-			m_oButton = optionButton::Option2;
+			m_oButton = optionButton::Option2;	//Changes the current button
 		}
 		break;
 	default:
@@ -157,8 +192,10 @@ void OptionsScreen::checkButtonSelected(GamePadState m_state, Xbox360Controller2
 	}
 }
 
+//Function to check if buttons are selected 
 void OptionsScreen::selectedButton(GamePadState m_state, Xbox360Controller2 m_controller)
 {
+	//Switch statement for which button has been picked
 	switch (m_oButton)
 	{
 	case Option1:
@@ -166,9 +203,9 @@ void OptionsScreen::selectedButton(GamePadState m_state, Xbox360Controller2 m_co
 	case Option2:
 		break;
 	case Option3:
-		if (m_state.A && !m_controller.m_previousState.A)
+		if (m_state.A && !m_controller.m_previousState.A)	//Checks if the back button has been pressed
 		{
-			m_transitionToMenu = true;
+			m_transitionToMenu = true;	//Starts the transition to the menu
 		}
 		break;
 	default:
