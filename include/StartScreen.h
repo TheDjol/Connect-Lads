a0c8d5c7fbdad1a025fc659ad2c73087151a0148
djoleary39@ymail.com <djoleary39@ymail.com>
2017-02-26 23:04:10 +0000

    Commented both StartScreen.cpp and StartScreen.h

--- include/StartScreen.h
+++ include/StartScreen.h
@@ -9,25 +9,31 @@ class Game;
 class startScreen
 {
 public:
+	/// <summary>
+	/// Constructor for the start screen,
+	/// Takes 2 arguments: the address of the game object and the address of the font file.
+	/// </summary>
+	/// <param name="game"></param>
+	/// <param name="font"></param>
 	startScreen(Game& game, sf::Font &font);
-	~startScreen();	//De-constructor for the splash screen
+	~startScreen();	// Deconstructor for the splash screen
 
-	void update(GamePadState m_state, sf::Time deltaTime);	//Updates the splash screen
-	void render(sf::RenderWindow& window);	//Draws the splash screen
+	void update(GamePadState m_state, sf::Time deltaTime);	// Updates the splash screen
+	void render(sf::RenderWindow& window);	// Draws the splash screen
 
 private:
-	Game *m_game;
-	bool m_startPressed = false;
-	sf::Sprite m_sprite;
-	sf::Sprite m_jack;
-	sf::Sprite m_john;
+	Game *m_game;	// A game object that points to the original.
+	bool m_startPressed = false;	// Determines whether the player has pressed start yet.
+	sf::Sprite m_sprite;	//	Sprite representing the background.
+	sf::Sprite m_jack;	// Sprite representing Jack's head.
+	sf::Sprite m_john;	// Sprite representing John's head.
 
-	sf::Texture m_texture;
-	sf::Texture m_jackTex;
-	sf::Texture m_johnTex;
-	bool m_timeStop = false;
-	sf::Time m_time;
-	const sf::Time TIME_PER_UPDATE = sf::microseconds(1500);
-	sf::Text m_text;
+	sf::Texture m_texture;	// The background texture.
+	sf::Texture m_jackTex;	// The texture that is Jack's head.
+	sf::Texture m_johnTex;	// The texture that is John's head.
+	bool m_timeStop = false;	// Detemines whether or not to stop increasing the variable m_time.
+	sf::Time m_time;	// Variable that keeps track of how much time has passed between each update.
+	const sf::Time TIME_PER_UPDATE = sf::microseconds(1500);	// the number that is added to m_time on each update.
+	sf::Text m_text;	// The text that is displayed to the screen.
 };
 #endif // !STARTSCREEN_H
--- src/StartScreen.cpp
+++ src/StartScreen.cpp
@@ -1,50 +1,50 @@
 #include "StartScreen.h"
 
 startScreen::startScreen(Game& game, sf::Font &font) :
-	m_game(&game),
+	m_game(&game),	// Sets the value in m_game to the address of the game argument.
 	m_text("Press Start", font, 32)
 {
-	if (!m_texture.loadFromFile(".\\resources\\images\\startBG.jpg"))
+	if (!m_texture.loadFromFile(".\\resources\\images\\startBG.jpg"))	// Checks that the texture has been loaded into the variable.
 	{
 		{
-			std::string s("Error loading texture");	//Outputs error message
-			throw std::exception(s.c_str());
+			std::string s("Error loading texture");	// Outputs error message.
+			throw std::exception(s.c_str());	// Opens a error window with the erro message displayed.
 		}
 	}
-	m_sprite.setTexture(m_texture);
+	m_sprite.setTexture(m_texture);	// Sets the texture of the sprite to the one that was just loaded in. 
 
-	if (!m_jackTex.loadFromFile(".\\resources\\images\\Jack.png"))
+	if (!m_jackTex.loadFromFile(".\\resources\\images\\Jack.png"))	// Checks that the texture has been loaded into the variable.
 	{
 		{
-			std::string s("Error loading texture");	//Outputs error message
-			throw std::exception(s.c_str());
+			std::string s("Error loading texture");	// Outputs error message.
+			throw std::exception(s.c_str());	// Opens a error window with the erro message displayed.
 		}
 	}
-	m_jack.setTexture(m_jackTex);
+	m_jack.setTexture(m_jackTex);	// Sets the texture of the sprite to the one that was just loaded in.
 
-	if (!m_johnTex.loadFromFile(".\\resources\\images\\John.png"))
+	if (!m_johnTex.loadFromFile(".\\resources\\images\\John.png"))	// Checks that the texture has been loaded into the variable.
 	{
 		{
-			std::string s("Error loading texture");	//Outputs error message
-			throw std::exception(s.c_str());
+			std::string s("Error loading texture");	// Outputs error message.
+			throw std::exception(s.c_str());	// Opens a error window with the erro message displayed.
 		}
 	}
-	m_john.setTexture(m_johnTex);
+	m_john.setTexture(m_johnTex);	// Sets the texture of the sprite to the one that was just loaded in.
 
-	m_sprite.setOrigin(450, 300);
-	m_sprite.setPosition(450, 300);
+	m_sprite.setOrigin(450, 300);	// Sets the origin to the centre of screen.
+	m_sprite.setPosition(450, 300);	// Sets the position of the sprite to the centre of the screen.
 
-	m_text.setColor(sf::Color(1, 1, 1));
-	m_text.setPosition(350, 500);
+	m_text.setColor(sf::Color(0, 0, 0));	// Sets the colour of the text to black.
+	m_text.setPosition(350, 500);	// Sets the position of the text to be bottom middle of the screen.
 
-	m_jack.setOrigin(25, 35);
-	m_john.setOrigin(25, 35);
+	m_jack.setOrigin(25, 35);	// Sets the origin of the sprite to be the centre of itself.
+	m_john.setOrigin(25, 35);	// Sets the origin of the sprite to be the centre of itself.
 
-	m_jack.setPosition(150, 165);
-	m_john.setPosition(750, 165);
+	m_jack.setPosition(150, 165);	// Sets Jack's head's position to the left of the title.
+	m_john.setPosition(750, 165);	// Sets John's head's position to the right of the title.
 
-	m_jack.scale(2, 2);
-	m_john.scale(2, 2);
+	m_jack.scale(2, 2);	// Scales Jack's head.
+	m_john.scale(2, 2);	// Scales John's head.
 	
 }
 
@@ -55,45 +55,47 @@ startScreen::~startScreen()
 
 void startScreen::update(GamePadState m_state, sf::Time deltaTime)
 {
-	m_john.setRotation(m_john.getRotation() - 0.2);
-	m_jack.setRotation(m_jack.getRotation() + 0.2);
-	if (m_state.Start || sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+	m_john.setRotation(m_john.getRotation() - 0.2);	// Rotates John's head.
+	m_jack.setRotation(m_jack.getRotation() + 0.2);	// Rotates Jack's head.
+
+	if (m_state.Start || sf::Keyboard::isKeyPressed(sf::Keyboard::Return))	// Checks if the player has hit either the start button or the return key. 
 	{
-		m_startPressed = true;
+		m_startPressed = true;	// Sets the bool to be true once the player presses either start or hits the return key.
 	}
-	else if (m_state.Back)
+	else if (m_state.Back)	// Checks if the player has pressed the back button.
 	{
-		exit(0);
+		exit(0);	// Closes the window.
 	}
 
-	if (m_startPressed)
+	if (m_startPressed)	// If the start button has been pressed this will execute.
 	{
-		if (!m_timeStop)
+		if (!m_timeStop)	// Checks if m_timeStop is false.
 		{
-			deltaTime.Zero;
-			m_timeStop = true;
+			deltaTime.Zero;	// resets deltaTime.
+			m_timeStop = true;	// Sets to true so that deltaTime won't restart again.
 		}
-		m_sprite.setRotation(m_sprite.getRotation() + 2);	//Rotates the sprite
-		m_sprite.scale(0.999, 0.999);	//Scales down the sprite
-		m_time += TIME_PER_UPDATE;	//Increments the counter
+
+		m_sprite.setRotation(m_sprite.getRotation() + 2);	// Rotates the sprite.
+		m_sprite.scale(0.999, 0.999);	// Scales down the sprite.
+		m_time += TIME_PER_UPDATE;	// Increments the counter.
 	}
 
-	if (m_time.asSeconds() > 3)
+	if (m_time.asSeconds() > 3)	// Checks that m_time is less then 3 seconds.
 	{
-		m_game->setGameState(MenuState::MenuScreen);
+		m_game->setGameState(MenuState::MenuScreen);	// Calls the method inside the game object to change game states.
 	}
 	
 }
 
 void startScreen::render(sf::RenderWindow& window)
 {
-	window.clear(sf::Color(220, 20, 60));	//Clears window
-	window.draw(m_sprite);		//Draws text to the screen
-	if (!m_startPressed)
+	window.clear(sf::Color(220, 20, 60));	// Clears window.
+	window.draw(m_sprite);		// Draws text to the screen.
+	if (!m_startPressed)	// Checks whether or not the player has hit start yet.
 	{
-		window.draw(m_jack);
-		window.draw(m_john);
-		window.draw(m_text);
+		window.draw(m_jack);	// Draws Jack's head to the screen.
+		window.draw(m_john);	// Draws John's head to the screen.
+		window.draw(m_text);	// Draws the text to the screen.
 	}
-	window.display();	//Displays the screen
+	window.display();	// Displays the screen.
 }
